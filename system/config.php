acd7e3d7ba144136f1579d943627a4885d07cb28
PDAD Soetta <pdad.soetta@gmail.com>
2019-08-15 11:35:26 +0700

    Added: several .htaccess to forbid folder access also to force https

--- system/config.php
+++ system/config.php
@@ -1,14 +1,14 @@
 <?php
 //path directives
 $config=array();
-$config['path']['base']='akang';	//used to exclude base_path from REQUEST_URI
+$config['path']['base']='';	//used to exclude base_path from REQUEST_URI
 $config['path']['models']='models';	//where the models are stored
 $config['path']['views']='views';	//where the views are stored
 $config['path']['controllers']='controllers';	//where the controllers are stored
 $config['path']['assets']='assets';	//where to store assets
 
 //databases
-$config['db']['hostname']='localhost';
+$config['db']['hostname']='192.168.146.248';
 $config['db']['username']='data_miner';
 $config['db']['password']='thel0newolf';
 $config['db']['database']='sapi';
